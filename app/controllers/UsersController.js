356661ed22408bf271358805254e8bdf4c8cb9f3
Sebasboll15 <36539445+Sebasboll15@users.noreply.github.com>
2018-07-08 17:24:49 -0500

    elimina y selecciona usuario

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -1,16 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../conexion/connWeb');
 
 router.route('/')
     .get(getRouteHandler)
     .post(postRouteHandler);
 
+router.route('/eliminar').delete(deleteUsuarioHandler);
+
+
+router.route('/cambiar-pass').get(getCambiarPassHandler)
+
 function getRouteHandler(req, res) {
-    //handle GET route here
+
+	consulta = "Select *, rowid from usuarios";
+	db.query(consulta).then(function(result){
+        usuarios = result ;
+		console.log('Se trajo los datos con exito', result);
+
+    	res.json(usuarios);
+    }, function(error){
+		console.log('No se pudo traer los datos', error);
+	})
+
 }
 
 function postRouteHandler(req, res) {
     //handle POST route here
 }
 
+function getCambiarPassHandler(req, res) {
+    res.send('Cambiado');
+}
+
+
+function deleteUsuarioHandler(req, res) {
+    
+	consulta = "DELETE FROM usuarios WHERE rowid = ? ";
+	db.query(consulta, [req.query.id]).then (function(result){
+		console.log('Eliminado', req.query);
+		res.send('Eliminado');
+	}, function(error){
+		console.log('No se pudo borrarlos datos', error);
+		res.status(400).send({ error: error })
+	})
+}
+
+
+
+                  
 module.exports = router;
