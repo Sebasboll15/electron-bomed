d8fcbb4559b462cc999991831b9bda08124f148d
erikesba <36581029+erikesba@users.noreply.github.com>
2018-07-11 09:49:06 -0500

    question controller

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -6,8 +6,7 @@ var router = express.Router();
 router.use('/usuarios', require('./UsersController'));
 router.use('/welcome', require('./WelcomeController'));
 router.use('/login', require('./LoginController'));
-
-//router.use('/preguntas', require('./QuestionsController'));
+router.use('/preguntas', require('./QuestionsController'));
   // router.use('/pruebas', require('./TestsController'));
 //router.use('/respuestas', require('./AnswersController'));
 
