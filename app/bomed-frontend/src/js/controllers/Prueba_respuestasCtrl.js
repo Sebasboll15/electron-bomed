58b1296d757987d32ea70d8980f04047ce7f0695
Joseth Guerrero <davidguerrero777@gmail.com>
2018-10-28 15:39:22 -0500

    Responden

--- app/bomed-frontend/src/js/controllers/Prueba_respuestasCtrl.js
+++ app/bomed-frontend/src/js/controllers/Prueba_respuestasCtrl.js
@@ -2,57 +2,55 @@ angular.module('app')
 
 .controller('Prueba_respuestasCtrl', function($scope, AuthServ, USER, $state, $http, toastr, MySocket){
 	$scope.usuario= USER ;
-    $scope.respuesta_llevada={};
-    $scope.indice_preg = 0;
+	$scope.respuesta_llevada={};
+	$scope.indice_preg = 0;
    
-    
-    $http.get('::Prueba_en_curso',  {params: { rowid: USER.prueba_id} }).then (function(result){
-            console.log(result);
-            $scope.prueba = result.data.prueba ;
-            $scope.preguntas = result.data.preguntas ;
-            console.log('Se trajo los datos con exito', result);
-        }, function(error){
-            console.log('No se pudo traer los datos', error);
+	
+	$http.get('::Prueba_en_curso',  {params: { rowid: USER.prueba_id} }).then (function(result){
+			$scope.prueba = result.data.prueba ;
+			$scope.preguntas = result.data.preguntas ;
+		}, function(error){
+			console.log('No se pudo traer los datos', error);
 
-        })
-                	
+		})
+					
 
-    $scope.seleccionarOpcion = function(opcion) {
+	$scope.seleccionarOpcion = function(opcion) {
 
-    	correcta = 0;
-    	if ($scope.preguntas[$scope.indice_preg].correcta == opcion ) {
-    		correcta = 1;
-    	} 
-        MySocket.emit('contesto_mal/bien', correcta);     
+		correcta = 0;
+		if ($scope.preguntas[$scope.indice_preg].correcta == opcion ) {
+			correcta = 1;
+		} 
+		MySocket.emit('contesto_mal/bien', {correcta: correcta});     
 
-        $http.get('::Prueba_en_curso/insertar', {params: {preg_id: $scope.preguntas[$scope.indice_preg].rowid, usuario_id: USER.rowid, opcion_elegida: opcion, correcta: correcta, duracion: 0 }  }).then (function(result){
-         
-        
-            console.log('Se insertaron los datos con exito', result);
-            
-         }, function(error){
-           console.log('No se pudo insertar los datos', error);
+		$http.get('::Prueba_en_curso/insertar', {params: {preg_id: $scope.preguntas[$scope.indice_preg].rowid, usuario_id: USER.rowid, opcion_elegida: opcion, correcta: correcta, duracion: 0 }  }).then (function(result){
+		 
+		
+			console.log('Se insertaron los datos con exito', result);
+			
+		 }, function(error){
+		   console.log('No se pudo insertar los datos', error);
 
-         })
+		 })
 
 
-        	if ($scope.prueba.dirigido == 1) {
+			if ($scope.prueba.dirigido == 1) {
 
-        	}else{
+			}else{
 
-        		$scope.indice_preg = $scope.indice_preg + 1;
+				$scope.indice_preg = $scope.indice_preg + 1;
 
-        	}
+			}
 
-        	if ($scope.indice_preg == $scope.preguntas.length) {
-        	
-        	 toastr.success('Prueba terminada');
-        	 $state.go('app.main');
-        	};
-        	
-                
+			if ($scope.indice_preg == $scope.preguntas.length) {
+			
+			 toastr.success('Prueba terminada');
+			 $state.go('app.main');
+			};
+			
+				
 
 
-    };    	
+	};    	
 
 });
